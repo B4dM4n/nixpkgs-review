12804217fcace27fe78acb1c7fa1f468fbea10bf
Fabian Möller <fabianm88@gmail.com>
2022-01-21 15:28:45 +0100

    Allow to specify package sets with -p

--- nixpkgs_review/nix/evalAttrs.nix
+++ nixpkgs_review/nix/evalAttrs.nix
@@ -1,20 +1,47 @@
-{ allowAliases ? false, attr-json }:
+{ allowAliases ? false, attr-json, maxDepth ? 1, recurseAll ? false }:
 
 with builtins;
 let
   pkgs = import <nixpkgs> { config = { checkMeta = true; allowUnfree = true; inherit allowAliases; }; };
-  lib = pkgs.lib;
+
+  inherit (pkgs.lib) attrByPath concatLists concatMap hasAttrByPath isAttrs isDerivation
+    listToAttrs mapAttrsToList nameValuePair splitString;
 
   attrs = fromJSON (readFile attr-json);
-  getProperties = name: let
-    attrPath = lib.splitString "." name;
-    pkg = lib.attrByPath attrPath null pkgs;
-    maybePath = builtins.tryEval "${pkg}";
-  in rec {
-    exists = lib.hasAttrByPath attrPath pkgs;
-    broken = !exists || !maybePath.success;
-    path = if !broken then maybePath.value else null;
-    drvPath = if !broken then pkg.drvPath else null;
-  };
+
+  recurseName = name:
+    recurseAttrs 1 name (attrByPath (splitString "." name) null pkgs);
+
+  recurseAttrs = depth: name: pkg:
+    let
+      maybeIsDerivation = tryEval (isDerivation pkg);
+      pkgIsDerivation = maybeIsDerivation.success && maybeIsDerivation.value;
+      pkgIsAttrs = maybeIsDerivation.success && isAttrs pkg;
+
+      pkgProperties = [ (getProperties name pkg maybeIsDerivation) ];
+    in
+    # pkg evaluated to a derivation, so return it's properties
+    if pkgIsDerivation then pkgProperties
+    # pkg evaluated to a set. Check the recursion filters and recurse for each attribute
+    else if pkgIsAttrs && (depth <= maxDepth) && pkg.recurseForDerivations or recurseAll then
+      concatLists (mapAttrsToList (n: v: recurseAttrs (depth + 1) "${name}.${n}" v) pkg)
+    # If we recursed already once, don't treat invalid values as broken
+    else if depth > 1 then [ ]
+    # Otherwise (for entries in the attr-json file) the properties will report the entry
+    # as non existent/invalid/broken
+    else pkgProperties;
+
+  getProperties = name: pkg: maybeIsDerivation:
+    let
+      attrPath = splitString "." name;
+      maybePath = tryEval "${pkg}";
+    in
+    nameValuePair name rec {
+      exists = hasAttrByPath attrPath pkgs;
+      invalid = maybeIsDerivation.success && !maybeIsDerivation.value;
+      broken = !exists || invalid || !maybePath.success;
+      path = if !broken then maybePath.value else null;
+      drvPath = if !broken then pkg.drvPath else null;
+    };
 in
-  pkgs.lib.genAttrs attrs getProperties
+listToAttrs (concatMap recurseName attrs)
