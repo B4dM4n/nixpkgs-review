138ea1addeb97d68d8728c3b8ef586278359c4db
Fabian Möller <fabianm88@gmail.com>
2025-09-17 08:25:21 +0200

    Perform nix_build without review shell
    Re-evaluating all attributes can significantly increase the memory consumption
    of `nix build` by several GB during the entire runtime of the process.
    Therefore, simply give the derivations that have already been evaluated to the
    process.

--- nixpkgs_review/review.py
+++ nixpkgs_review/review.py
@@ -466,12 +466,9 @@ class Review:
         return nix_build(
             packages_per_system,
             args,
-            self.builddir.path,
-            self.local_system,
             self.allow,
             self.build_graph,
             self.builddir.nix_path,
-            self.nixpkgs_config,
             self.num_eval_workers,
             self.max_memory_size,
         )
