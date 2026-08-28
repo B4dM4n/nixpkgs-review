487887ddd7916c7a5933e9db1c858f46b39bc3dc
Fabian Möller <fabianm88@gmail.com>
2026-08-28 16:06:00 +0200

    Don't evaluate packages in `nix build`
    Re-evaluating all attributes can significantly increase the memory consumption
    of `nix build` by several GB during the entire runtime of the process and also
    can take some time for large reviews.
    
    The required derivations are already known from `multi_system_eval`, so pass
    them directly to `nix build`.
    
    Sadly, we can't pass derivations or store paths directly to
    `mkShell`/`nix-shell`, so we need to turn the required outputs to store paths
    with `builtins.storePath` and pass them to `mkShell`.

--- nixpkgs_review/review.py
+++ nixpkgs_review/review.py
@@ -679,14 +679,10 @@ class Review:
         if not self.shell_options.no_shell:
             shell_config = ShellConfig(
                 cache_directory=path,
-                local_system=self.build_config.local_system,
                 build_graph=self.shell_options.build_graph,
                 nix_path=self.builddir.nix_path,
-                nixpkgs_config=self.build_config.nixpkgs_config,
-                nixpkgs_overlay=self.builddir.overlay.path,
                 run=self.shell_options.run,
                 sandbox=self.shell_options.sandbox,
-                pkgs=self.build_config.pkgs,
             )
             nix_shell(report.built_packages(), shell_config)
 
@@ -1013,7 +1009,6 @@ def build_config_from_args(
     return BuildConfig(
         allow=allow,
         nix_path=nix_path,
-        local_system=current_system(),
         nixpkgs_config=nixpkgs_config,
         num_eval_workers=args.num_eval_workers,
         max_memory_size=args.max_memory_size,
